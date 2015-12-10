10df6a1ad6166d9e2bd0621f56b590088f714bb3
Heda Hahn <hedassecret@gmail.com>
2015-12-10 13:09:53 -0800

    Great day

--- creativity.rb
+++ creativity.rb
@@ -1,7 +1,7 @@
 def heda_hahn
 
   ARGV.each do |arg|
-     puts "Welcome to Heda Hahn Designs #{arg}!"
+     puts "Welcome to Heda Hahn Designs, today is going to be a GREAT day #{arg}!"
    end
 
 end
